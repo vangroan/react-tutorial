c14feff9ef28901aae211b4ea6df25cd013837c1
Willem Victor <wimpievictor@gmail.com>
2021-03-29 18:13:58 +0200

    Added redux tutorial base project

---
+++ redux-tutorial/src/components/App.js
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+import { Button, Container, Header, Segment } from 'semantic-ui-react';
+
+const formatter = new Intl.NumberFormat('en-ZA', {
+  style: 'currency',
+  currency: 'ZAR',
+});
+
+function App() {
+  // -------
+  //  State
+  // -------
+  const initial = 10000;
+  const [balance, setBalance] = useState(initial);
+
+  // --------
+  //  Action
+  // --------
+  const deposit = () => setBalance(balance + 100);
+  const withdraw = () => setBalance(balance - 100);
+
+  // ------
+  //  View
+  // ------
+  return (
+    <div>
+      <Container text style={{paddingTop: "2em"}}>
+        <Segment.Group>
+          <Segment padded="very" textAlign="center">
+            <Header as="h2">
+              {formatter.format(balance)}
+            <Header.Subheader>Balance</Header.Subheader>
+            </Header>
+          </Segment>
+          <Segment padded="very" textAlign="center">
+            <Button content="Deposit" primary icon="plus circle" onClick={deposit} />
+            <Button content="Withdraw" secondary icon="minus circle" onClick={withdraw} />
+          </Segment>
+        </Segment.Group>
+      </Container>
+    </div>
+  );
+}
+
+export default App;
