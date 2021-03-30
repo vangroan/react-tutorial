f8aa515105712ce78b637c8b6b0e1f6fa0d389bc
Willem Victor <wimpievictor@gmail.com>
2021-03-30 17:51:02 +0200

    redux-thunk: Server calls

--- redux-tutorial/src/app/components/App.js
+++ redux-tutorial/src/app/components/App.js
@@ -1,6 +1,8 @@
-import { useState } from 'react';
+import { useEffect } from 'react';
 import { useDispatch, useSelector, shallowEqual } from 'react-redux';
 import { Button, Container, Header, Segment } from 'semantic-ui-react';
+
+import { postDeposit, postWithdraw, getBalance } from 'app/action';
 
 const formatter = new Intl.NumberFormat('en-ZA', {
   style: 'currency',
@@ -14,13 +16,17 @@ function App() {
   // const initial = 10000;
   // const [balance, setBalance] = useState(initial);
   const balance = useSelector(state => state.balance, shallowEqual);
+  const loading = useSelector(state => state.loading, shallowEqual);
 
   // --------
   //  Action
   // --------
   const dispatch = useDispatch();
-  const deposit = () => dispatch({ type: 'app/deposit', payload: 100 }); //setBalance(balance + 100);
-  const withdraw = () => dispatch({ type: 'app/withdraw', payload: 100 });// setBalance(balance - 100);
+  // const deposit = () => dispatch({ type: 'app/deposit', payload: 100 });
+  // const withdraw = () => dispatch({ type: 'app/withdraw', payload: 100 });
+  const deposit = () => dispatch(postDeposit(100));
+  const withdraw = () => dispatch(postWithdraw(100));
+  useEffect(() => dispatch(getBalance()), []);
 
   // ------
   //  View
@@ -36,8 +42,8 @@ function App() {
             </Header>
           </Segment>
           <Segment padded="very" textAlign="center">
-            <Button content="Deposit" primary icon="plus circle" onClick={deposit} />
-            <Button content="Withdraw" secondary icon="minus circle" onClick={withdraw} />
+            <Button content="Deposit" primary icon="plus circle" onClick={deposit} loading={loading} />
+            <Button content="Withdraw" secondary icon="minus circle" onClick={withdraw} loading={loading} />
           </Segment>
         </Segment.Group>
       </Container>
--- redux-tutorial/src/app/reducer.js
+++ redux-tutorial/src/app/reducer.js
@@ -1,5 +1,6 @@
 
 const initialState = {
+  loading: false,
   balance: 10000,
 };
 
@@ -9,6 +10,46 @@ const reducer = (state = initialState, action) => {
       return { ...state, balance: state.balance + action.payload };
     case 'app/withdraw':
       return { ...state, balance: state.balance - action.payload };
+
+    // ----------------------------------
+    // Balance
+    case 'app/balance/INIT':
+      return { ...state, loading: true };
+    case 'app/balance/SUCCESS':
+      return {
+        ...state,
+        loading: false,
+        balance: action.payload.balance
+      };
+    case 'app/balance/ERROR':
+      return { ...state, loading: false };
+
+    // ----------------------------------
+    // Deposit
+    case 'app/deposit/INIT':
+      return { ...state, loading: true };
+    case 'app/deposit/SUCCESS':
+      return {
+        ...state,
+        loading: false,
+        balance: action.payload.balance
+      };
+    case 'app/deposit/ERROR':
+      return { ...state, loading: false };
+
+    // ----------------------------------
+    // Withdraw
+    case 'app/withdraw/INIT':
+      return { ...state, loading: true };
+    case 'app/withdraw/SUCCESS':
+      return {
+        ...state,
+        loading: false,
+        balance: action.payload.balance
+      };
+    case 'app/withdraw/ERROR':
+      return { ...state, loading: false };
+
     default:
       return state;
   }
